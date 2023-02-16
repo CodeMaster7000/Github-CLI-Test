404abefc5f0d92d1c739b47f20946c11bbf4441e
CodeMaster7000 <95772109+CodeMaster7000@users.noreply.github.com>
2023-02-16 16:33:07 +0000

    Update CLITest.cs

---
+++ CLITest.cs
@@ -0,0 +1,9 @@
+using System;
+namespace CLITest {
+	class CLITest {
+		static void Main(string[] args) {
+			Console.WriteLine("This is a GitHub CLI test repository.");
+			Console.ReadKey();
+		}
+	}
+}
